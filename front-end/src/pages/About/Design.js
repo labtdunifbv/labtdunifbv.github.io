d02829a70a06491cf9736eecf3b4d41b9f8e22dd
KaioHenrique21 <202051797991@alunos.unifbv.edu.br>
2021-11-18 20:47:52 -0300

    atualizacao do home e sobre

--- front-end/src/pages/About/Design.js
+++ front-end/src/pages/About/Design.js
@@ -14,9 +14,26 @@ function Design() {
             <h1>NÚCLEO DESIGN</h1>
             <br></br>
             <br></br>
-            <h3>Designer 1</h3>
+            <h2>IDEIA - ESCRITÓRIO EXPERIMENTAL DE DESIGN</h2>
             <br></br>
-            <h3>Designer 2</h3>
+            <h3>
+            Prof. Arthur de Oliveira Filho
+            </h3>
+            <h3>
+            Beatriz Mota
+            </h3>
+            <h3>
+            Julia Belian
+            </h3>
+            <h3>
+            Luana Celine
+            </h3>
+            <h3>
+            Assíria Rocha
+            </h3>
+            <h3>
+            Rayna Cavalcante
+            </h3>
             <br></br>
             <br></br>
             <br></br>
--- front-end/src/pages/About/Transformacao.js
+++ front-end/src/pages/About/Transformacao.js
@@ -14,9 +14,35 @@ function Transformacao() {
             <h1>NÚCLEO DE TRANSFORMAÇÃO DIGITAL</h1>
             <br></br>
             <br></br>
-            <h3>TI 1</h3>
+            <h2>LAB TD - LABORATÓRIO TRANSFORMAÇÃO DIGITAL</h2>
             <br></br>
-            <h3>TI 2</h3>
+            <h3>
+            Prof. Andrea Raymundo Balle
+            </h3>
+            <h3>
+            Kaio Henrique Pacheco Dos Santos Oliveira
+            </h3>
+            <h3>
+            Agostinho Henrique Teixeira de Santana Neto
+            </h3>
+            <h3>
+            Misael Sebastião do Nascimento
+            </h3>
+            <h3>
+            Thiago Henrique Melo da Silva
+            </h3>
+            <h3>
+            José Carlos de Medeiros Filho
+            </h3>
+            <h3>
+            João Vitor de Souza Vieira
+            </h3>
+            <h3>
+            Priscila Maria Ferreira de Araújo
+            </h3>
+            <h3>
+            Pedro Henrique Van-Lume Viana
+            </h3>
             <br></br>
             <br></br>
             <br></br>
