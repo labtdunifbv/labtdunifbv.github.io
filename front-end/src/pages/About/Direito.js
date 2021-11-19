32ba2e275995caa63047469099814964a8ac1100
priscila199 <primvjc@hotmail.com>
2021-11-19 15:57:31 -0300

    atualizacao nucleo juridico

--- front-end/src/pages/About/Direito.js
+++ front-end/src/pages/About/Direito.js
@@ -14,9 +14,39 @@ function Direito() {
             <h1>NÚCLEO JURÍDICO</h1>
             <br></br>
             <br></br>
-            <h3>Jurídico 1</h3>
-            <br></br>
-            <h3>Jurídico 2</h3>
+            <h3>
+            Prof. Alessandra Prôa Greenhalgh de Oliveira
+            </h3>
+            <h3>
+            Maria Heloiza Chaves
+            </h3>
+            <h3>
+            Evelyn Vitória Paz de Albuquerque
+            </h3>
+            <h3>
+            Cristina Helena Barreto de Alcantara
+            </h3>
+            <h3>
+            Elves José Ximenes Bandeira de Almeida
+            </h3>
+            <h3>
+            Ana Marcely Vidal de Lima 
+            </h3>
+            <h3>
+            Rayan Jesus dos Santos 
+            </h3>
+            <h3>
+            Camila de Andrade Silva
+            </h3>
+            <h3>
+            Alycia Gabrielle de Lima
+            </h3>
+            <h3>
+            Camila Alves Sales Caminha Silveira
+            </h3>
+            <h3>
+            Jessyca Vitoria Maria Francisco Silva
+            </h3>
             <br></br>
             <br></br>
             <br></br>
