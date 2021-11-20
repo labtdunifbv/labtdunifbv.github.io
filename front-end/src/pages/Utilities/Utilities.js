1599d1cbe4fe8ec88721fca2479ef704f4a6454f
KaioHenrique21 <202051797991@alunos.unifbv.edu.br>
2021-11-19 21:12:34 -0300

    atualizacao scroll utilidades

--- front-end/src/pages/Utilities/Utilities.js
+++ front-end/src/pages/Utilities/Utilities.js
@@ -8,9 +8,9 @@ function Utilities() {
     return(
         <div className="container-utilities">  
             <h1 className="title-utilites">{title}</h1>
-            <br></br>
+           
             <h3>Link: <a href="http://www.planalto.gov.br/ccivil_03/_ato2015-2018/2018/lei/l13709.htm">Lei LGPD Completa</a></h3>
-            <br></br>
+          
             <Footer verification={title}/>
         </div>
     );
